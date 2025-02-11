44bcedb5c2be53dfcba3bf3ff97d029333569f12
Laxystem <the@laxla.quest>
2025-02-11 09:39:35 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,12 @@ allprojects {
     group = GROUP
     version = _version
 }
+
+
+
+if (hasProperty("buildScan")) {
+    extensions.findByName("buildScan")?.withGroovyBuilder {
+        setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+        setProperty("termsOfServiceAgree", "yes")
+    }
+}
