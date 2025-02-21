da8f53fd46ffd7a539210c7106436f22c40ae75b
Laxystem <the@laxla.quest>
2025-02-21 18:01:13 +0200

    Added more SuspendCloseable tests

--- util/src/commonTest/kotlin/SuspendCloseables.kt
+++ util/src/commonTest/kotlin/SuspendCloseables.kt
@@ -1,13 +1,12 @@
 package quest.laxla.spock
 
 import kotlinx.coroutines.test.runTest
-import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.*
 
 
 class SuspendCloseables {
 	@Test
-	fun closure() = runTest {
+	fun `errorless execution`() = runTest {
 		var closed = false
 		var executed = false
 
@@ -18,5 +17,59 @@ class SuspendCloseables {
 		}
 
 		assertTrue(closed && executed)
+	}
+
+	@Test
+	fun `exception in usage`() = runTest {
+		var closed = false
+		val closeable = SuspendCloseable {
+			closed = true
+		}
+
+		assertFailsWith(IllegalStateException::class) {
+			closeable.use {
+				error("error!")
+			}
+		}
+
+		assertTrue(closed)
+	}
+
+	@Test
+	fun `exception in closure`() = runTest {
+		var executed = false
+		val closeable = SuspendCloseable {
+			error("error in closure!")
+		}
+
+		assertFailsWith(IllegalStateException::class) {
+			closeable.use {
+				executed = true
+			}
+		}
+
+		assertTrue(executed)
+	}
+
+	@Test
+	@Suppress("VariableInitializerIsRedundant")
+	fun `exception in closure and usage`() = runTest {
+		val closeable = SuspendCloseable {
+			error("error in closure!")
+		}
+
+		var exception: Throwable? = null
+
+		try {
+			closeable.use {
+				null!!
+			}
+		} catch (e: Throwable) {
+			exception = e
+		}
+
+		assertNotNull(exception)
+		assertIs<NullPointerException>(exception)
+		assertTrue(exception.suppressedExceptions.any { it is IllegalStateException })
 	}
 }
