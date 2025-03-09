33d3da6a4ef5b370dc10bf1b81ba0e559cf7dedb
Laxystem <the@laxla.quest>
2025-03-09 21:12:13 +0200

    Deleted unused property

--- core/src/commonMain/kotlin/Meshes.kt
+++
@@ -1,8 +0,0 @@
-package quest.laxla.spock
-
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.map
-
-public val <Vertex : Any> Mesh<Vertex>.orderedVertices: Flow<Vertex>
-	get() = indices.asFlow().map { vertices[it.toInt()] }
